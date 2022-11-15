911d1b55f01e2bd6eac3e63869fdc175319ea2b3
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2022-11-15 19:39:53 +1100

    take full pae content hrml

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const puppeteer = require("puppeteer");
 async function run() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://www.traversymedia.com");
+  await page.goto("https://ashishdasnurkar.com");
 
-  await page.screenshot({ path: "example.png", fullPage: true });
+  //   await page.screenshot({ path: "example.png", fullPage: true });
+  const html = await page.content();
+  console.log(html);
 
   await browser.close();
 }
