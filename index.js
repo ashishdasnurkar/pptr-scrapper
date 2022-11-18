f004a5349dbf614426e57bfa7cc11faf3bb5a36a
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2022-11-18 18:40:13 +1100

    print courses

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const puppeteer = require("puppeteer");
 async function run() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://ashishdasnurkar.com");
+  await page.goto("https://traversymedia.com");
 
   //   await page.screenshot({ path: "example.png", fullPage: true });
   //   const html = await page.content();
@@ -11,10 +11,21 @@ async function run() {
   const title = await page.evaluate(() => document.title);
   console.log(title);
   //  Get all links
-  const links = await page.evaluate(() =>
-    Array.from(document.querySelectorAll("a"), (e) => e.href)
+  //   const links = await page.evaluate(() =>
+  //     Array.from(document.querySelectorAll("a"), (e) => e.href)
+  //   );
+  //   console.log(links);
+
+  const courses = await page.$$eval("#courses .card", (elements) =>
+    elements.map((e) => ({
+      title: e.querySelector(".card-body h3").innerText,
+      level: e.querySelector(".card-body .level").innerText,
+      url: e.querySelector(".card-footer a").href,
+      promo: e.querySelector(".card-footer .promo-code .promo").innerText,
+    }))
   );
-  console.log(links);
+
+  console.log(courses);
 
   await browser.close();
 }
